43d703d8307041dc583f97cdf7a19b9b2c31823a
RefaelSachevski <refael32901@gmail.com>
2022-09-13 13:51:36 +0300

    Update Hangman.js

--- Hangman.js
+++ Hangman.js
@@ -1,36 +1,39 @@
 var Word = "ketchup"; 
-var underScore = '_';
+var underScore = "_";
 var wordArray = Word.split("");
 var allowedMistakes = 4;
 var gameON = true;
 var badGuesses = [ ];
-var goodGuesses = [""];
+var goodGuesses = [];
 var result = [ ];
-var totalGuesses = 0 ;
-console.log(wordArray)
+
 
 for (var i = 0; i < Word.toString().length; i++){ // loop which gives me underscore's according to the length of the word
-    goodGuesses += underScore
+    goodGuesses += underScore;
 }
-console.log(goodGuesses)
-
+var spaces = Array.from(goodGuesses)
+console.log("Welcome you have started the Hangman game, good luck")
 while ( gameON ){  // first loop which starts the game 
     var userInput = prompt("Please enter a Character");
+    if (userInput.toString().length > 1){
+        console.log("You have mistakenly enterd : " + userInput + " ,which is more then one charcter, please try again")
+    }
     if (wordArray.toString().length == result.toString().length){
-        totalGuesses = badGuesses.toString().length + badGuesses.toString().length
-        console.log("Good job man, you have finished the game and it only took you: " + totalGuesses)
+        console.log("Good job man, you have finished the game")
         break;  // stops the entire game
     }
     if ( wordArray.includes(userInput) ) {
         for ( j = 0; j <= wordArray.toString().length; j ++){
             var index = wordArray.indexOf(userInput)
             if (index == j){
-                totalGuesses = totalGuesses + 1
-                result[index] = userInput
-                console.log(result)
+                result[index] = userInput;
+                // console.log(result.toString().replace(/,/g,'_'));
+                spaces[index] = userInput
+                console.log(spaces.toString().replace(/,/g,' '));
+
             }
         }        
-    } else {
+    } else if (userInput.toString().length == 1) {
         badGuesses += userInput;
         console.log(badGuesses)
         if (badGuesses.toString().length == allowedMistakes){
